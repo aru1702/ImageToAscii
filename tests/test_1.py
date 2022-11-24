2d36d1a73c063f724897392671462130b2753dbd
Muhamad Aldy B <muhamad.aldy_bintang.lw2@is.naist.jp>
2022-11-24 17:22:01 +0900

    add tox ini

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,11 +1,8 @@
 import unittest
-from unittest.mock import patch
 from ImageToAscii.ImageToAscii import main as ITAMain
 
 
 class TestImageToAscii(unittest.TestCase):
-
-    # @patch('builtins.input', return_value=image_path)
     def test_image(self):
         image_path = 'ImageToAscii/naist_logo_sq.jpg'
         result = ITAMain(150, image_path)
